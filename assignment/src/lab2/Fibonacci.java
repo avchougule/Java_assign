3eda2f69b611ac2f765375501cac974ac2dd911f
avchougule <akshaychougule878@gmail.com>
2025-06-19 21:55:19 +0530

    Second commit

---
+++ assignment/src/lab2/Fibonacci.java
@@ -0,0 +1,10 @@
+//Generate the Fib… series 1 1 2 3 5 8 13
+package lab2;
+import java.util.*;
+public class Fibonacci 
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Enter the number for series:");
+	}
+}
